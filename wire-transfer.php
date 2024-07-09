5177e6784410a0e7f70179ee1a87982888f033c9
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2024-07-09 12:03:02 -0700

    Create wire-transfer.php

---
+++ wire-transfer.php
@@ -0,0 +1,90 @@
+<html lang="en" dir="ltr"><!-- Mirrored from themes.pixelstrap.net/pwa/mpay/help.html by HTTrack Website Copier/3.x [XR&CO'2017], Sun, 05 Nov 2023 05:25:27 GMT --><head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="mpay">
+  <meta name="keywords" content="mpay">
+  <meta name="author" content="mpay">
+  <link rel="manifest" href="manifest.json">
+  <link rel="icon" href="assets/images/logo/favicon.png" type="image/x-icon">
+  <title>Wire Transfer | QFS FIRM</title>
+  <link rel="apple-touch-icon" href="assets/images/logo/favicon.png">
+  <meta name="theme-color" content="#122636">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="mpay">
+  <meta name="msapplication-TileImage" content="assets/images/logo/favicon.png">
+  <meta name="msapplication-TileColor" content="#FFFFFF">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <!--Google font-->
+  <link rel="preconnect" href="../../../external.html?link=https://fonts.googleapis.com/">
+  <link rel="preconnect" href="../../../external.html?link=https://fonts.gstatic.com/" crossorigin="">
+  <link href="../../../external.html?link=https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&amp;display=swap" rel="stylesheet">
+
+  <!-- bootstrap css -->
+  <link rel="stylesheet" id="rtl-link" type="text/css" href="assets/css/vendors/bootstrap.min.css">
+
+  <!-- swiper css -->
+  <link rel="stylesheet" type="text/css" href="assets/css/vendors/swiper-bundle.min.css">
+
+  <!-- Theme css -->
+  <link rel="stylesheet" id="change-link" type="text/css" href="assets/css/style.css">
+  
+  <!-- Smartsupp Live Chat script -->
+<script type="text/javascript" charset="utf-8" async="" src="https://www.smartsuppchat.com/loader.js?"></script><script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '2924ddeb399034af673a7042631ae2c70526ef26';
+window.smartsupp||(function(d) {
+	var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+	s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+	c.type='text/javascript';c.charset='utf-8';c.async=true;
+	c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+
+</head>
+
+<body class="auth-body">
+  <!-- header starts -->
+  <div class="auth-header">
+    <div class="help-head d-flex">
+      <a href="account"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left back-btn"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg> </a>
+      <h2>Wire Transfer</h2>
+    </div>
+    <div class="head-img text-center">
+      <img class="img-fluid img2" src="assets/images/authentication/help.svg" alt="v1">
+    </div>
+  </div>
+  <!-- header end -->
+
+  <!-- help section start -->
+  <form class="auth-form" target="_blank">
+    <div class="custom-container">
+      <div class="help-center">
+        <h2 class="fw-semibold">Want to do a wire transfer?</h2><br>
+        <div class="accordion accordion-flush help-accordion" id="accordionFlushExample">
+          1. Create an account on Crypto.com <br><br>
+          2. Click on link wallet and link your Crypto.com wallet to QFS. <br><br>
+          3. Your wire transfer details will be shown on your dashboard.
+          
+        </div>
+      </div>
+    </div>
+  </form>
+  <!-- help section end -->
+
+  <!-- feather js -->
+  <script src="assets/js/feather.min.js"></script>
+  <script src="assets/js/custom-feather.js"></script>
+
+  <!-- bootstrap js -->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+  <!-- script js -->
+  <script src="assets/js/script.js"></script>
+
+
+
+
+<iframe id="chat-application-iframe" title="Smartsupp" aria-hidden="true" style="display: block; position: fixed; top: 0px; left: 0px; width: 1px; height: 1px; opacity: 0; border: none; z-index: -1; pointer-events: none;"></iframe><div id="smartsupp-widget-container">  <div style="border-radius: 9999px; box-shadow: rgba(0, 0, 0, 0.06) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 2px 32px; height: 56px; position: fixed; bottom: 24px; left: initial; right: 12px; z-index: 10000000; width: 109px;" data-testid="widgetButtonFrame"><iframe id="widgetButtonFrame" title="Smartsupp widget button" style="position: absolute; width: 100%; height: 100%; border: none; display: block; text-align: left; margin: 0px; padding: 0px; top: 0px; left: 0px; opacity: 1;" allowfullscreen="" scrolling="no"></iframe></div>  </div></body><!-- Mirrored from themes.pixelstrap.net/pwa/mpay/help.html by HTTrack Website Copier/3.x [XR&CO'2017], Sun, 05 Nov 2023 05:25:28 GMT --></html>
